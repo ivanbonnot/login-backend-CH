cf8f43b01b23b32498c427a1fe0fb3423a649a99
ivanbonnot <ivanbonnot@gmail.com>
2023-02-27 17:39:51 -0300

    listo y probado el fork

--- src/controllers/random.js
+++ src/controllers/random.js
@@ -1,5 +1,5 @@
 class Random {
-    getNumers = (amount) => {
+    getNumbers = (amount) => {
 
         const numerosAleatorios = {};
 
--- src/routes/api/randoms.js
+++ src/routes/api/randoms.js
@@ -1,15 +1,27 @@
 const { Router } = require('express')
 const randomsRouter = Router()
-const randomController = require('../../controllers/random')
 const { fork } = require('child_process');
 
-randomsRouter.get('/:cant',(req, res) => {
+randomsRouter.get('/:cant', (req, res) => {
     const { cant } = req.params
-    res.send(randomController.getNumers(cant))
+    const child = fork('./src/helpers/randomNumber.js')
+
+    child.send({ cant: `${cant}` })
+
+    child.on('message', (resultado) => {
+        res.send(resultado)
+    })
 })
 
-randomsRouter.get('/',(req, res) => {
-    res.send(randomController.getNumers(100000000))
+randomsRouter.get('/', (req, res) => {
+
+    const child = fork('./src/helpers/randomNumber.js')
+
+    child.send({ cant: `${1000000000}` })
+
+    child.on('message', (resultado) => {
+        res.send(resultado)
+    })
 })
 
 module.exports = randomsRouter
