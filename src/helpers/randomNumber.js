9055c5716aa5b2e4cabe82c7aa503c94ba38bc03
ivanbonnot <ivanbonnot@gmail.com>
2023-02-26 21:40:40 -0300

    falta el fork

---
+++ src/helpers/randomNumber.js
@@ -0,0 +1,2 @@
+const random = require('../controllers/random')
+
--- src/routes/api/randoms.js
+++ src/routes/api/randoms.js
@@ -1,8 +1,15 @@
 const { Router } = require('express')
 const randomsRouter = Router()
+const randomController = require('../../controllers/random')
+const { fork } = require('child_process');
+
+randomsRouter.get('/:cant',(req, res) => {
+    const { cant } = req.params
+    res.send(randomController.getNumers(cant))
+})
 
 randomsRouter.get('/',(req, res) => {
-
+    res.send(randomController.getNumers(100000000))
 })
 
 module.exports = randomsRouter
